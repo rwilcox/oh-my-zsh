9e25e137da691b4c7d4f9eae6c782846ab100d4b
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-06-28 21:40:40 -0400

    initial commit of radiantcms plugin

---
+++ plugins/radiantcms/radiantcms.plugin.zsh
@@ -0,0 +1,5 @@
+alias rads="script/server -e production"
+alias radc="RAILS_ENV=production script/console"
+alias radup="rake production radiant:extensions:update_all"
+alias radmigrate="rake production db:migrate:extensions"
+
